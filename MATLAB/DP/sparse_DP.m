c6eea73954d64bd0348ce9c05ac6c613a13d700d
rbds <rschur18@gmail.com>
2015-12-17 14:31:56 -0500

    minor update

--- MATLAB/DP/sparse_DP.m
+++ MATLAB/DP/sparse_DP.m
@@ -3,11 +3,13 @@
 clear
 close all
 
+tic
+
 P_tr_thresh = 0.4;
 
 %build adjacency matrix
 %square grid, connected right and down
-s =2;
+s =4;
 V = s^2;
 N = V^2;
 i_vals = [];
@@ -17,7 +19,7 @@ vals = [];
 
 minimum = 0.9;
 maximum = 1.0;
-P_tr = (maximum - minimum)*rand(V, 1) + minimum;%Prob. of traverse associated with each node. sqrt is to bias towards higher values.
+P_tr = (maximum - minimum)*rand(V, 1) + minimum; %Prob. of traverse associated with each node. sqrt is to bias towards higher values.
 costs = 10*abs(randn(V,1));
 
 for i = 1:V    %letter
@@ -89,26 +91,19 @@ for i = 1:N
 end
 gplot(adj, coords, '*-')
 
-
-ind_last = V^4 - V - 1; %index of the end node
-col_last = N;   %column of last node
-row_last = N-V-1;   %row of last node
-V_last = [row_last, col_last ];
-num = nnz(adj); %total number of non-zero values in adjacency matrix
 [adj_i, adj_j, adj_v] = find(adj); %access rows and columns of adjacency matrix.
 
 P_tr(end) = 1;
-%d{i}(1) is minimum cost, d{i}(2) is total P_tr, d{i}(3) is parent node
-d{N,1} = [];
+%d{i,1}(1) is minimum cost, d{i,1}(2) is total P_tr, d{i,2}(j,:) is path
+%represented by that node.
+d{N,2} = [];
 for i=(N-V+1):(N) %create d vector to store cost, P_tr, parents for each entry in adj.
-   d{i,1} = [0 1 i];
+   d{i,1} = [0 1];
+   d{i,2} = [i];
 end
 
 %calculate costs, P_tr for current vertex
 for i=N:-1:1 %counting back from last populated column in adjacency matrix,
-% list_of_nodes_to_visit = V;
-% while (~isempty(list_of_nodes_to_visit)  %While the list of nodes to try is not empty
-%     [col, list_of_nodes_to_visit] = pop(list_of_nodes_to_visit);         %column of current vertex
     col = i;  
         V_col = mod(col, V);
         if (V_col==0) V_col = V; end
@@ -118,15 +113,15 @@ for i=N:-1:1 %counting back from last populated column in adjacency matrix,
         row = conns(j);             %row of current connection
         V_row = mod(row, V);
         if (V_row==0) V_row = V; end 
-        l = size(d{i}(:,1),1);
+        l = size(d{i,1}(:,1),1);
         p = size(options, 1);
-        options(end+1:end+l,1) = d{i}(:,1) + adj(row,col); %cost of each one of d{i} = adj(row,col)
+        options(end+1:end+l,1) = d{i,1}(:,1) + adj(row,col); %cost of each one of d{i} = adj(row,col)
                
         if p>0
-           options((p+1):end, 2) = d{i}(:,2)*P_tr(V_row); %calculate P_tr for options
+           options((p+1):end, 2) = d{i,1}(:,2)*P_tr(V_row); %calculate P_tr for options
            options((p+1):end, 3) = i; %list parent node for each option.
         else
-           options(:, 2) = d{i}(:,2)*P_tr(V_row); %calculate P_tr for options
+           options(:, 2) = d{i,1}(:,2)*P_tr(V_row); %calculate P_tr for options
            options(:, 3) = i; %list parent node for each option.
         end 
         
@@ -136,9 +131,9 @@ for i=N:-1:1 %counting back from last populated column in adjacency matrix,
            end
         end
     front = options(1,:);
-%     d{conns(j)}(end+1,:) = front;  %populate cost, P_tr, parent.
+ %populate cost, P_tr, parent.
    
-% inner front      
+% inner front -- Check that this is working
     for k = 2:size(options,1)
        list1 = find(front(:,1) > options(k,1));       %find list of points with greater cost
        list2 = find(front(:,2) > options(k,2));       %find list of points with greater P_tr
@@ -166,67 +161,12 @@ for i=N:-1:1 %counting back from last populated column in adjacency matrix,
         end
     end
     len = size(front, 1);
-    d{conns(j)}(end+1:end+len,:) = front;
+    d{conns(j),1}(end+1:end+len,:) = front;
     end
 end
 
-% t = 1;
-% % [val, ind] = min(d{1}(:,1)); %find minimum cost parent at node 1.
-% best_path = ones(V, 1);
-% n = N;
-% l=1;
-% for i=1:V
-%     while (t<n)
-%         [~, ind] = min(d{t}(:,1));    %find minimum cost path at node t
-%         best_path(i, l+1) = d{t}(ind,3);
-%         t = best_path(i, l+1);
-%         l = l+1;
-%     end
-%     n= n-V;
-%     t = 1;
-%     l=1;
-% end
 
-
-back_ptrs = d{1}(:,3);
-g = [1 0 0 length(back_ptrs)];
-j=2;
-for i = 1:length(back_ptrs)-1
-   if (back_ptrs(i)~=back_ptrs(i+1))
-      g(j) = i+1;
-      j = j+1;
-   end
-end
-
-best_cost = [1000 1000 1000];
-best_Ptr =  [0 0 0];
-best_ind = [0 0 0];
-for i=1:3
-    for j = g(i):g(i+1)-1
-        if (d{1}(j,1) < best_cost(i))
-           best_cost(i) =  d{1}(j,1);
-           best_Ptr(i) = d{1}(j,2);
-           best_ind(i) = j;
-           
-        end
-    end 
-end
-
-% best_path = [1 1 1];
-% t = 1;
-% l=1;
-% for i=1:3
-%    ind = best_ind(i);
-%    while mod(t,V)
-%         best_path(l+1,i) = d{t}(ind,3);
-%         t = best_path(l+1,i);
-%         l = l+1;
-%         [~, ind] = min(d{t}(:,1));    %find minimum cost path at node t
-%    end
-%     t = 1;
-%     l=1;
-% end
-%    
+time = toc
    
 [~, ind] = min(d{1}(:,1));
 cost = d{1}(ind,1)
