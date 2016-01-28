44650d04d0bcf483d65697df5e03108ba03c8dbd
clyman23 <clyman23@gmail.com>
2016-01-28 16:56:25 -0500

    More driving charge experiment data

--- Jackie/Charge Experiment/Robot_Drive_Serial.py
+++ Jackie/Charge Experiment/Robot_Drive_Serial.py
@@ -12,7 +12,7 @@ import serial
 import time
 import csv
 
-data_file = open('data_20in4.csv', 'wb') #CSV file to store data sent by robot
+data_file = open('data_30in5.csv', 'wb') #CSV file to store data sent by robot
 writer = csv.writer(data_file)
 writer.writerow(('index', 'encoder1', 'encoder2', 'turn angle degrees', 'ms', 'mA'))
 
@@ -32,7 +32,7 @@ brake90 = '$00013' #brake and turn 90 degrees
 brake120 = '$00014' #brake and turn 120 degrees
 brake180 = '$00016'
 brake = '$00000' #brake only
-drive = '$12000' #drive 20 inches
+drive = '$13000' #drive 20 inches
 
 '''Need to clean up function'''
 def read_robot(): 
