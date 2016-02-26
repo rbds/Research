be74072ed06787345fa95dfc64622daaad5f9159
rbds <rschur18@gmail.com>
2016-02-26 18:49:34 -0500

    plot data from linoleum charge tests

--- Jackie/Charge_Experiment/plot_charge.py
+++ Jackie/Charge_Experiment/plot_charge.py
@@ -6,37 +6,65 @@ This is a temporary script file.
 """
 
 from matplotlib import pyplot
-from matplotlib import rcParams, cm
+from matplotlib import rcParams
 rcParams['font.family'] = 'serif'
 rcParams['font.size'] = 16
 import csv
 import numpy
 
+lengths = numpy.array([13, 23, 33, 43, 53])
+#lengths = numpy.array([13])
+mn = numpy.zeros(6)
+st = numpy.zeros(6)
 
-f = open('23inchesFeb26_00.csv', 'rb')
-reader = csv.reader(f)
-rownum = 0
-last_row = 72
+for ind, j  in enumerate(lengths):
+    charge_totals = numpy.array([])
+    
+    for i in range(0,20):
+        filename = str(j) + 'inchesFeb26_0' + str(i) + '.csv'
+        with open(filename, 'rb') as f:
+            mycsv = csv.reader(f)
+            mycsv = list(mycsv)
+            row_count = len(mycsv) - 2
+            start_at = float(mycsv[1][0])
+        
+        f = open(filename, 'rb')
+        reader = csv.reader(f)
+        rownum = 0
+        
+        encoder = numpy.array([])
+        seconds = numpy.array([])
+        amps = numpy.array([])
+        
+        for r in reader:
+            #print(r[1])
+            if rownum>0 and rownum<row_count:
+                encoder = numpy.append(encoder, float(r[0]) - start_at)
+                seconds = numpy.append(seconds, float(r[1]))
+                amps = numpy.append(amps, float(r[2]))
+            rownum +=1
+        f.close()
+        charge = seconds*amps
+        tot = numpy.cumsum(charge)
+        charge_totals = numpy.append(charge_totals, tot[-1])
 
-encoder = numpy.zeros(last_row)
-seconds = numpy.zeros(last_row)
-amps = numpy.zeros(last_row)
+    #print(charge_totals)
+    #print(numpy.size(charge_totals))    
+    mn[ind+1] = numpy.mean(charge_totals)
+    st[ind+1] = numpy.std(charge_totals)
+    
+l = numpy.append([0], lengths) 
+m, b = numpy.polyfit(l, mn, 1)
+pyplot.figure()
+pyplot.plot(l, m*l+b)
+pyplot.errorbar(l, mn, yerr = st, fmt = 'x')
+pyplot.title('Charge vs. distance')
+pyplot.xlabel('inches travelled')
+pyplot.ylabel('charge used (Amp-seconds)')
 
-for r in reader:
-    #print( r[1])
-    #print(rownum)
-    if rownum< last_row and rownum>0:
-        encoder[rownum] = r[0]
-        seconds[rownum] = r[1]
-        amps[rownum] = r[2]
-    rownum +=1
-	
+pyplot.savefig('charge_v_distance.png')
 
-charge = seconds*amps
-tot = numpy.cumsum(charge)
-#tot = numpy.zeros_like(charge)
-
-pyplot.plot(encoder, tot , 'b-');
-pyplot.xlabel('distance(encoder counts)');
-pyplot.ylabel('charge C' );
+#    pyplot.plot(encoder, tot , 'b-');
+#    pyplot.xlabel('distance(encoder counts)');
+#    pyplot.ylabel('charge C' );
 
