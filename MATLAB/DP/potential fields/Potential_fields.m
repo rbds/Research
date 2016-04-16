8e6a8473154c08fe43bf7ac1e600fb4e0f5ab3b5
rbds <rschur18@gmail.com>
2016-04-16 17:58:05 -0400

    create potential field method

---
+++ MATLAB/DP/potential fields/Potential_fields.m
@@ -0,0 +1,83 @@
+%Potential Field Method
+
+%%%%%%%%%Define a map with obstacles, start, and target locations
+% close all
+clf
+figure(1)
+hold on
+clear
+
+
+%create obstacles by code         
+course = [0 0 45 33];
+numberOfObstacles = 5;
+s = [];
+map = [];
+% Draw Course    
+obst = draw_obstacles(numberOfObstacles, course);
+
+% Set Start and Goal locations
+p_start = [5;28];
+p_goal = [41; 6];
+
+% Parameters 
+param.res = 0.25;            %Resolution of intermediate points. Must be <0.25 for InCollision_Edge to run.
+param.thresh = 12;           %Bias towards the goal starting at this distance
+param.maxiters = 100;      %Cap on iterations to run RRT
+param.RRTstarrad = 15;      %Maximum length of lines redrawn by RRT*
+param.goalbias = .95;        %Probability of checking the goal as p_new
+param.maxpathlength = 20;    %Maximum length of any path segment.
+param.sensor_range = 6;
+%param.smoothiters = 150;    %Number of iterations for smoothing algorithm
+goal.r = .5;            %radius of goal
+robot.r = 0.75;
+robot.t = 0;
+
+robot.p = p_start;               %set robot position to x_start
+
+circle(p_start(1,1),p_start(2,1),goal.r,'g');               %draw the location of x_start and x_goal
+circle(p_goal(1,1),p_goal(2,1),goal.r,'g');
+
+ka = .2;
+kr = .4;
+q_thresh = 10;
+
+dt = .2;
+c = 1;
+
+
+%%%%%%%%%%%%%%while robot position != goal:
+h = draw_robot(robot);
+while norm(robot.p - p_goal) > robot.r+goal.r
+    %%%%%%%%%Define robot position
+    map = [];
+    %%%%%%%%%%%do a sensor sweep
+    [ map, s ] = sensor( robot, obst, map, s, param.sensor_range);
+
+    %%%%%%%%%%%Find potential function
+        %attractive potential
+        dU_a = ka*(robot.p - p_goal);
+%         plot([robot.p(1), robot.p(1)-dU_a(1)], [robot.p(2), robot.p(2)-dU_a(2)], 'g' )
+   
+        %repulsive potential
+        dU_r = zeros(length(map),2);
+        for i=1:length(map)
+            d_to_obst = norm(-map(i).p + robot.p);
+            del_r = (-map(i).p + robot.p)/norm(-map(i).p + robot.p);
+            dU_r(i,:) = kr*(1/q_thresh - 1/d_to_obst)*1/d_to_obst^2*del_r;
+%             plot([robot.p(1), robot.p(1)+dU_r(i,1)], [robot.p(2), robot.p(2)+dU_r(i,2)], 'r' )
+        end
+        
+    %%%%%%%%%%%Find gradient
+        F = sum([-dU_a';-dU_r],1);
+
+    %%%%%%%%%%%% Move robot for one timestep
+        old_p = robot.p;
+        plot(robot.p(1), robot.p(2), 'bx')
+        robot.p = robot.p + dt*F'/c;
+        plot([old_p(1), robot.p(1)],[old_p(2), robot.p(2)],'g', 'LineWidth', 3)
+        set(h, 'Visible', 'off')
+        h = draw_robot(robot);
+        set(h, 'Visible', 'on')
+        drawnow
+end
---
+++ MATLAB/DP/potential fields/sensor.m
@@ -0,0 +1,58 @@
+function [ map, s ] = sensor( robot, obst, map, s, rad)
+
+%Sensor Sweep
+num_readings = 25;
+
+theta = linspace(0, 2*pi, num_readings);
+x1(1:num_readings) = robot.p(1);
+y1(1:num_readings) = robot.p(2);
+% rad = 10;
+x2 = rad*cos(theta) + x1;       %rad is an arbitrarily large number. This may need to be changed to reflect course size
+y2 = rad*sin(theta) + y1;
+lines(:, 1) = x1;
+lines(:,2) = y1;
+lines(:, 3) = x2;
+lines(:,4) = y2;
+
+for i = 1:length(x1)
+ set(s, 'visible','off')
+ s = plot([x1(i);x2(i)],[y1(i);y2(i)], 'k');
+ set(s, 'visible', 'on')
+ pause(.001);
+%  M(end+1) = getframe;
+end
+[~, xlocations, ylocations] = sensorSweep(lines,obst);
+
+for i = 1:size(xlocations,1)
+    x = xlocations(i,:);        %xlocations is the N1xN2 matrix where i,j is the x coordinate of the intersections between X1(i) and X2(j)
+    y = ylocations(i,:);        %x,y are the coordinates of the intersection between sensor line and obstacles
+    z = cat(1,x,y);             %Z is the concat coordinates of the intersections of sensor line with each obstacles
+    for k = 1:length(z)
+        if xlocations(i,k) == 0
+            d(k) = 100000000; %random big number
+        else
+        d(k) = norm(z(:,k) - robot.p);  %distance from robot.p to each obstacle intersection.
+        end
+    end
+     closest = find(d== min(d));        %find closest obstacle for each sensor line.
+    if size(closest,2) == 1
+           map(end +1).p(1,1) = x(closest); %add closest intersection to the map.
+           map(end).p(2,1) = y(closest);
+    end
+end
+
+for i = 1:length(map)
+    
+plot(map(i).p(1), map(i).p(2), 'r*', 'LineWidth', 3);    %plot the coordinates of the full map.
+% M(end+1) = getframe;  
+end
+% M(end+1) = getframe;
+
+% 
+
+end
+    
+
+
+
+
