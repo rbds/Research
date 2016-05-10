a87a7963e28072d081d403dd334ab685dc648e23
rbds <rschur18@gmail.com>
2016-05-10 09:38:40 -0400

    stash some edits, new images

--- MATLAB/DP/plot_maps.m
+++ MATLAB/DP/plot_maps.m
@@ -8,7 +8,7 @@ subplot(1,2,1)
 for i = 1:V
     v = [ coords(i,1)-0.5 coords(i,2)-0.5; coords(i,1)-0.5 coords(i,2)+0.5; coords(i,1)+0.5 coords(i,2)+0.5; coords(i,1)+0.5  coords(i,2)-0.5 ];
 %     patch('Faces', [1 2 3 4], 'Vertices', v, 'FaceColor', [0 0 1], 'FaceAlpha', costs(i).^.5)
-    patch('Faces', [1 2 3 4], 'Vertices', v, 'FaceColor', [costs(i).^.5 costs(i)^.5 costs(i)^.5])
+    patch('Faces', [1 2 3 4], 'Vertices', v, 'FaceColor', [1-costs(i).^.5 1-costs(i)^.5 1-costs(i)^.5])
 end
 
 axis off
@@ -18,16 +18,17 @@ title('Cost Map')
 
 shades = linspace(0, 1, 64);
 newmap = [shades' shades' shades'];
-colormap(newmap);   %activate it
-colorbar('location', 'southoutside')
-
+colormap((newmap));   %activate it
+% colorbar('location', 'southoutside', 'YDir', 'reverse')
+h = colorbar('location', 'southoutside');
+set(h, 'YDir', 'reverse')
 subplot(1,2,2)
 
 % figure
 for i = 1:V
     v = [ coords(i,1)-0.5 coords(i,2)-0.5; coords(i,1)-0.5 coords(i,2)+0.5; coords(i,1)+0.5 coords(i,2)+0.5; coords(i,1)+0.5  coords(i,2)-0.5 ];
 %     patch('Faces', [1 2 3 4], 'Vertices', v, 'FaceColor', [0 0 1], 'FaceAlpha', costs(i).^.5)
-    patch('Faces', [1 2 3 4], 'Vertices', v, 'FaceColor', [P_tr(i).^2 P_tr(i).^2 P_tr(i).^2])
+    patch('Faces', [1 2 3 4], 'Vertices', v, 'FaceColor', [1-P_tr(i).^2 1-P_tr(i).^2 1-P_tr(i).^2])
 end
 
 axis off
@@ -37,8 +38,9 @@ title('P_{tr} Map')
 
 shades = linspace(0, 1, 64);
 newmap = [shades' shades' shades'];
-colormap(newmap);   %activate it
-colorbar('location', 'southoutside')
+colormap(flipud(newmap));   %activate it
+h = colorbar('location', 'southoutside');
+set(h, 'YDir', 'reverse')
 
 end
 
